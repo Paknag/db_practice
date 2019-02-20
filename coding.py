2f2015cfb35cdb550739e01b0c083d85f519d9fe
Will Dove <soap@apple.com>
2019-02-20 07:43:08 -0800

    Updated to have a record again

--- coding.py
+++ coding.py
@@ -3,30 +3,32 @@ import helpers
 
 helpers.connect_db()
 
-pd = PracticeData()
-pd.namef = 'will'
+# pd = PracticeData()
+# pd.namef = 'will'
+# pd.namel = 'dove'
+# pd.note = 'Things and stuff'
+# pd.save()
 
 
+user_table_headers = PracticeData().find(1)._attributes
 
-# user_table_headers = PracticeData().find(1)._attributes
+def return_a_detail(grabbed_detail, value):
+    row = PracticeData.find(value)
+    if grabbed_detail.lower() != 'all':
+        try: 
+            detail = getattr(row, grabbed_detail.lower())
+            print(f'The answer is {detail.title()}')
+        except AttributeError: 
+            print(f'There is no "{grabbed_detail}" attribute.')
+    else: 
+        buffer = ""
+        for column, detail in user_table_headers.items():
+            buffer += f'{column.upper()}: {detail} \n'
+        print(buffer)
 
-# def return_a_detail(grabbed_detail, value):
-#     row = PracticeData.find(value)
-#     if grabbed_detail.lower() != 'all':
-#         try: 
-#             detail = getattr(row, grabbed_detail.lower())
-#             print(f'The answer is {detail}')
-#         except AttributeError: 
-#             print(f'There is no "{grabbed_detail}" attribute.')
-#     else: 
-#         buffer = ""
-#         for column, detail in user_table_headers.items():
-#             buffer += f'{column.upper()}: {detail} \n'
-#         print(buffer)
-
-# while True:
-#     column = input('What is being searched?: ')
-#     if column == '' or column.isspace() == '': break
-#     from_id = input('What record number?: ')
-#     if from_id == '' or from_id.isspace() == '': break
-#     if column and from_id: return_a_detail(column, from_id)
+while True:
+    column = input('What is being searched?: ')
+    if column == '' or column.isspace() == '': break
+    from_id = input('What record number?: ')
+    if from_id == '' or from_id.isspace() == '': break
+    if column and from_id: return_a_detail(column, from_id)
